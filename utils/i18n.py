18db13f2e0552b254e703d3ddbb329934ee8967f
mishatunikov <michael.tunikov@gmail.com>
2025-04-10 00:19:49 +0300

    Разработана функция для получения объекта TranslatorHub - доступа к текстам проекта.

---
+++ utils/i18n.py
@@ -0,0 +1,26 @@
+from fluent_compiler.bundle import FluentBundle
+
+from fluentogram import TranslatorHub, FluentTranslator
+
+
+def create_translator_hub() -> TranslatorHub:
+    """Создает объект TranslatorHub."""
+    translator_hub = TranslatorHub(
+        {
+            'ru': ('ru',),
+        },
+        [
+            FluentTranslator(
+                locale='ru',
+                translator=FluentBundle.from_files(
+                    locale='ru-RU',
+                    filenames=[
+                        'locales/ru/LC_MESSAGES/lexicon.ftl',
+                    ],
+                ),
+            ),
+        ],
+        root_locale='ru',
+    )
+
+    return translator_hub
