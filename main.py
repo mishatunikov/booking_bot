72e1de6f34ae626e94a328f279ff7a94d27c199c
mishatunikov <michael.tunikov@gmail.com>
2025-04-10 00:49:57 +0300

    Выполнено подключение диалога и роутера.

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiogram_dialog import setup_dialogs
 from fluentogram import TranslatorHub
 
 from config_data.config import Config, load_config
 from fsm import storage
 from middlewares.outer import TranslatorRunnerMiddleware
 from utils.i18n import create_translator_hub
+from handlers.user_handlers import router as user_router
+from dialogs.booking_creation import booking_creation
 
 logger = logging.getLogger(__name__)
 
@@ -36,6 +39,8 @@ async def main():
     )
 
     logger.info('Подключаем роутеры')
+    dp.include_routers(user_router, booking_creation)
+    setup_dialogs(dp)
 
     logger.info('Подключаем миддлвари')
     dp.update.middleware(TranslatorRunnerMiddleware())
