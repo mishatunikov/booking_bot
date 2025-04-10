e3770d95c7ec96b751578a2a879cd71f0ffbd748
mishatunikov <michael.tunikov@gmail.com>
2025-04-10 23:36:46 +0300

    Добавлен фильтр корректности введенного имени.

---
+++ dialogs/booking_creation/filters.py
@@ -0,0 +1,18 @@
+from aiogram.types import Message
+
+from aiogram.filters import BaseFilter
+
+from dialogs.booking_creation.consts import MAX_NAME_LENGTH
+
+
+class CorrectInput(BaseFilter):
+
+    async def __call__(self, message: Message):
+
+        if any(char.isdigit() for char in message.text):
+            return False
+
+        if len(message.text) > MAX_NAME_LENGTH:
+            return False
+
+        return True
