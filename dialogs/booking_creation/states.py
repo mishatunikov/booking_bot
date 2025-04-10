e9903ee769da032a9b29ced37a53fd017411b96c
mishatunikov <michael.tunikov@gmail.com>
2025-04-10 23:35:14 +0300

    Добавлены новые состояния BookingCreationSG.

--- dialogs/booking_creation/states.py
+++ dialogs/booking_creation/states.py
@@ -4,3 +4,6 @@ from aiogram.fsm.state import State, StatesGroup
 class BookingCreationSG(StatesGroup):
     select_date = State()
     select_time = State()
+    input_name = State()
+    select_persons_count = State()
+    confirmation = State()
