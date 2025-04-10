d78aee95fc2608742013dd3b63045a2680ea5197
mishatunikov <michael.tunikov@gmail.com>
2025-04-10 20:26:32 +0300

    Добавлена логика пагинации при выборе времени.

--- dialogs/booking_creation/getters.py
+++ dialogs/booking_creation/getters.py
@@ -4,7 +4,7 @@ from aiogram.types import User
 from aiogram_dialog import DialogManager
 from fluentogram import TranslatorRunner
 
-from .consts import MAX_NUMBER_OF_DATES, DATES_ON_PAGE
+from dialogs.booking_creation import consts
 
 
 async def select_date_getter(
@@ -14,14 +14,16 @@ async def select_date_getter(
     **kwargs,
 ):
     page = dialog_manager.start_data.get('dates_page')
-    days_for_booking = []
-    days_off = DATES_ON_PAGE * (page + 1)
+    days_on_page = []
+    days_off = consts.DATES_ON_PAGE * (page + 1)
 
-    if (end_range := (page + 1) * DATES_ON_PAGE) > MAX_NUMBER_OF_DATES:
-        end_range = MAX_NUMBER_OF_DATES
+    if (
+        end_range := (page + 1) * consts.DATES_ON_PAGE
+    ) > consts.MAX_NUMBER_OF_DATES:
+        end_range = consts.MAX_NUMBER_OF_DATES
 
-    for i in range(DATES_ON_PAGE * page, end_range):
-        days_for_booking.append(
+    for i in range(consts.DATES_ON_PAGE * page, end_range):
+        days_on_page.append(
             (
                 (datetime.today() + timedelta(days=i)).strftime('%d.%m'),
                 (datetime.today() + timedelta(days=i)).date(),
@@ -30,9 +32,34 @@ async def select_date_getter(
     return {
         'select_date_text': i18n.booking.creation.select.date(),
         'main_menu': i18n.back(),
-        'days_for_booking': days_for_booking,
-        'have_next': days_off < MAX_NUMBER_OF_DATES,
-        'have_previous': days_off > DATES_ON_PAGE,
+        'days_for_booking': days_on_page,
+        'have_next': days_off < consts.MAX_NUMBER_OF_DATES,
+        'have_previous': days_off > consts.DATES_ON_PAGE,
+        'next': i18n.next(),
+        'previous': i18n.previous(),
+    }
+
+
+async def select_time_getter(
+    dialog_manager: DialogManager,
+    event_from_user: User,
+    i18n: TranslatorRunner,
+    **kwargs,
+):
+    page = dialog_manager.start_data.get('times_page')
+
+    times_on_page = consts.BUTTONS_TIMES[
+        page * consts.TIMES_ON_PAGE : (page + 1) * consts.TIMES_ON_PAGE
+    ]
+
+    return {
+        'select_time_text': i18n.booking.creation.select.time(),
+        'back': i18n.back(),
+        'times_for_booking': times_on_page,
+        'have_next': times_on_page[-1][1] + consts.TIMES_ON_PAGE
+        < consts.TIME_CLOSE,
+        'have_previous': times_on_page[0][1] - consts.TIMES_ON_PAGE
+        >= consts.TIME_OPEN,
         'next': i18n.next(),
         'previous': i18n.previous(),
     }
