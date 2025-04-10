5e4618f4b959fd74badd9689387a5f264cd87190
mishatunikov <michael.tunikov@gmail.com>
2025-04-10 22:08:40 +0300

    Использование sqlite вместо postgres.

--- db/connection.py
+++ db/connection.py
@@ -1,8 +1,4 @@
-import dotenv
-import os
-
 from sqlalchemy.ext.asyncio import create_async_engine
 
-dotenv.load_dotenv()
 
-engine = create_async_engine(os.getenv('DATABASE_URL'), echo=True)
+engine = create_async_engine('sqlite+aiosqlite:///text.db', echo=True)
