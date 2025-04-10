349c25dc82beebe3713044931ff09c0d0d4d7e0b
mishatunikov <michael.tunikov@gmail.com>
2025-04-10 14:44:24 +0300

    Добавлен порт в настройках redis.

--- fsm/fsm.py
+++ fsm/fsm.py
@@ -1,10 +1,10 @@
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.base import DefaultKeyBuilder
+from aiogram.fsm.storage.memory import MemoryStorage
 from redis.asyncio import Redis
 from aiogram.fsm.storage.redis import RedisStorage
 
-
-redis = Redis(host='localhost')
+redis = Redis(host='localhost', port=6379)
 storage = RedisStorage(
     redis=redis, key_builder=DefaultKeyBuilder(with_destiny=True)
 )
