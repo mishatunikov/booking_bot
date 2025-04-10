d39122fde98c9cb75a9166b960f366ae9074b626
mishatunikov <michael.tunikov@gmail.com>
2025-04-10 23:37:02 +0300

    Добавлено новое окно - ввод имени.

--- dialogs/booking_creation/dialog.py
+++ dialogs/booking_creation/dialog.py
@@ -1,4 +1,6 @@
+from aiogram.enums import ContentType
 from aiogram_dialog import Dialog, Window
+from aiogram_dialog.widgets.input import MessageInput
 from aiogram_dialog.widgets.kbd import SwitchTo, Back
 from aiogram_dialog.widgets.text import Const, Format
 from aiogram_dialog.widgets.kbd import (
@@ -10,11 +12,19 @@ from aiogram_dialog.widgets.kbd import (
     Group,
 )
 
+from dialogs.booking_creation.filters import CorrectInput
 from dialogs.booking_creation.getters import (
     select_date_getter,
     select_time_getter,
+    ask_name_getter,
 )
-from dialogs.booking_creation.handlers import change_page
+from dialogs.booking_creation.handlers import (
+    change_page,
+    confirm_booking,
+    correct_input,
+    incorrect_input,
+    not_text_input,
+)
 from dialogs.booking_creation.states import BookingCreationSG
 from dialogs.booking_creation.handlers import select_date, select_time
 from dialogs.booking_creation import consts
@@ -77,4 +87,31 @@ booking_creation = Dialog(
         state=BookingCreationSG.select_time,
         getter=select_time_getter,
     ),
+    Window(
+        Format('{ask_name}'),
+        MessageInput(
+            func=correct_input,
+            content_types=ContentType.TEXT,
+            filter=CorrectInput(),
+        ),
+        MessageInput(func=incorrect_input, content_types=ContentType.TEXT),
+        MessageInput(func=not_text_input, content_types=ContentType.ANY),
+        SwitchTo(
+            Format('{back}'),
+            state=BookingCreationSG.select_time,
+            id='cancel_input_name',
+        ),
+        getter=ask_name_getter,
+        state=BookingCreationSG.input_name,
+    ),
+    # Window(
+    #     Format('{confirm_booking_text}'),
+    #     Button(Format('{confirm}'), on_click=confirm_booking, id='click_yes'),
+    #     SwitchTo(
+    #         Format('{not_confirm}'),
+    #         state=BookingCreationSG.select_time,
+    #         id='click_not',
+    #     ),
+    #     state=BookingCreationSG.confirmation,
+    # ),
 )
