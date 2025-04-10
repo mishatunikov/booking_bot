5f60db77cfc14d35e950f672b6a5a123acaa26f9
mishatunikov <michael.tunikov@gmail.com>
2025-04-10 14:41:32 +0300

    Создан getter для стартового диалога.

---
+++ dialogs/starting_dialog/getters.py
@@ -0,0 +1,22 @@
+from aiogram.types import User
+from aiogram_dialog import DialogManager
+from fluentogram import TranslatorRunner
+
+
+async def introduction_getter(
+    dialog_manager: DialogManager,
+    event_from_user: User,
+    i18n: TranslatorRunner,
+    **kwargs,
+):
+    data = int(bool(dialog_manager.start_data))
+    menu_text = i18n.main.menu(
+        start_data=data,
+        name=event_from_user.first_name,
+    )
+
+    return {
+        'menu_text': menu_text,
+        'booking_check_button': i18n.start.booking.check(),
+        'booking_creation_button': i18n.start.booking.creation(),
+    }
