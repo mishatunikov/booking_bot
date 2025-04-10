7342775092b054f5d5d43094c3d0833341127edb
mishatunikov <michael.tunikov@gmail.com>
2025-04-10 14:42:52 +0300

    Разработан хэндлер обработки команды /start.

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,12 +1,20 @@
 from aiogram import Router
 from aiogram.filters import CommandStart
+from aiogram.types import Message
 from aiogram_dialog import DialogManager, StartMode
+
+from dialogs.starting_dialog.states import StartSG
 
 router = Router()
 
 
 @router.message(CommandStart())
 async def command_start(
+    message: Message,
     dialog_manager: DialogManager,
 ):
-    await dialog_manager.start(state=..., mode=StartMode.RESET_STACK)
+    await dialog_manager.start(
+        state=StartSG.intro,
+        mode=StartMode.RESET_STACK,
+        data={'first_open': True},
+    )
