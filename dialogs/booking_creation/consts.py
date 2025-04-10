6014bdd176f68c45272d064d8e28acaec6f5c5e4
mishatunikov <michael.tunikov@gmail.com>
2025-04-10 20:26:03 +0300

    Добавлены константы для работы с окном выбора времени.

--- dialogs/booking_creation/consts.py
+++ dialogs/booking_creation/consts.py
@@ -1,2 +1,12 @@
 DATES_ON_PAGE = 3
 MAX_NUMBER_OF_DATES = 7
+
+TIMES_ON_PAGE = 6
+TIME_OPEN = 10
+TIME_CLOSE = 22
+
+WIDTH_TIME_BUTTONS = 3
+
+BUTTONS_TIMES: list[tuple[str, int]] = [
+    (f'{time}:00', time) for time in range(TIME_OPEN, TIME_CLOSE)
+]
