cc406c3dd87f66fc8ebab29778ea266a5edf44c9
mishatunikov <michael.tunikov@gmail.com>
2025-04-11 22:37:49 +0300

    Добавлено окно справочной информации.

--- dialogs/starting_dialog/dialog.py
+++ dialogs/starting_dialog/dialog.py
@@ -1,8 +1,11 @@
 from aiogram_dialog import Dialog, Window
-from aiogram_dialog.widgets.kbd import Button
+from aiogram_dialog.widgets.kbd import Button, Back, Next
 from aiogram_dialog.widgets.text import Format
 
-from dialogs.starting_dialog.getters import introduction_getter
+from dialogs.starting_dialog.getters import (
+    introduction_getter,
+    reference_getter,
+)
 from dialogs.starting_dialog.handlers import start_next_dialog
 from dialogs.starting_dialog.states import StartSG
 
@@ -19,7 +22,14 @@ starting_dialog = Dialog(
             id='booking_check',
             on_click=start_next_dialog,
         ),
+        Next(Format('{reference_button}')),
         state=StartSG.intro,
         getter=introduction_getter,
+    ),
+    Window(
+        Format('{reference_text}'),
+        Back(Format('{back_to_main_menu}')),
+        getter=reference_getter,
+        state=StartSG.reference,
     ),
 )
