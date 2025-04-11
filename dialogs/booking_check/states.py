4c21a9a94db98d282190caba48e3a975b952533e
mishatunikov <michael.tunikov@gmail.com>
2025-04-11 15:27:14 +0300

    Добавлены новые состояния для BookingCheckSG.

--- dialogs/booking_check/states.py
+++ dialogs/booking_check/states.py
@@ -3,3 +3,5 @@ from aiogram.fsm.state import State, StatesGroup
 
 class BookingCheckSG(StatesGroup):
     main_page = State()
+    confirm_cancel_booking = State()
+    success_cancel_booking = State()
