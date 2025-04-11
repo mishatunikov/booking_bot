fbfa66192657ae5f69c6338cc26f9b5b61be99f9
mishatunikov <michael.tunikov@gmail.com>
2025-04-11 22:06:04 +0300

    Добавлена логика проверки доступности времени для текущего дня.

--- dialogs/booking_creation/getters.py
+++ dialogs/booking_creation/getters.py
@@ -14,7 +14,7 @@ async def select_date_getter(
     page = dialog_manager.start_data.get('dates_page')
     days_on_page = []
     days_off = consts.DATES_ON_PAGE * (page + 1)
-
+    date_diff = datetime.now().hour + 1 >= consts.TIME_CLOSE
     if (
         end_range := (page + 1) * consts.DATES_ON_PAGE
     ) > consts.MAX_NUMBER_OF_DATES:
@@ -23,8 +23,10 @@ async def select_date_getter(
     for i in range(consts.DATES_ON_PAGE * page, end_range):
         days_on_page.append(
             (
-                (datetime.today() + timedelta(days=i)).strftime('%d.%m'),
-                (datetime.today() + timedelta(days=i)).date(),
+                (datetime.today() + timedelta(days=i + date_diff)).strftime(
+                    consts.DATE_PATTERN_DISPLAY
+                ),
+                (datetime.today() + timedelta(days=i + date_diff)).date(),
             )
         )
     return {
@@ -44,18 +46,32 @@ async def select_time_getter(
     **kwargs,
 ):
     page = dialog_manager.start_data.get('times_page')
+    start_range_time = consts.TIME_OPEN
+    current_hour = datetime.now().hour
+    reserved_date = datetime.strptime(
+        dialog_manager.dialog_data.get('reserved_date'), consts.DATE_PATTERN
+    )
+    if reserved_date.day == datetime.now().day and (
+        current_hour > consts.TIME_OPEN
+    ):
+        start_range_time = current_hour + 1
 
-    times_on_page = consts.BUTTONS_TIMES[
-        page * consts.TIMES_ON_PAGE : (page + 1) * consts.TIMES_ON_PAGE
+    buttons_ids = [
+        (f'{time}:00', time)
+        for time in range(start_range_time, consts.TIME_CLOSE)
     ]
     return {
         'select_time_text': i18n.booking.creation.select.time(),
         'back': i18n.back(),
-        'times_for_booking': times_on_page,
-        'have_next': times_on_page[-1][1] + consts.TIMES_ON_PAGE
+        'times_for_booking': buttons_ids,
+        'have_next': buttons_ids[-1][1] + consts.TIMES_ON_PAGE
         < consts.TIME_CLOSE,
-        'have_previous': times_on_page[0][1] - consts.TIMES_ON_PAGE
-        >= consts.TIME_OPEN,
+        'have_previous': all(
+            [
+                buttons_ids[0][1] - consts.TIMES_ON_PAGE >= consts.TIME_OPEN,
+                buttons_ids[0][1] > current_hour + 1,
+            ]
+        ),
         'next': i18n.next(),
         'previous': i18n.previous(),
     }
