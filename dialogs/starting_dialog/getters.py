de88350abe30d538e61fd7ce9ad1692b80749a52
mishatunikov <michael.tunikov@gmail.com>
2025-04-11 22:37:24 +0300

    Добавлен геттер для справочной информации.

--- dialogs/starting_dialog/getters.py
+++ dialogs/starting_dialog/getters.py
@@ -2,6 +2,8 @@ from aiogram.types import User
 from aiogram_dialog import DialogManager
 from fluentogram import TranslatorRunner
 
+from dialogs.consts import TIME_CLOSE, TIME_OPEN
+
 
 async def introduction_getter(
     dialog_manager: DialogManager,
@@ -22,4 +24,17 @@ async def introduction_getter(
         'menu_text': menu_text,
         'booking_check_button': i18n.start.booking.check(),
         'booking_creation_button': i18n.start.booking.creation(),
+        'reference_button': i18n.reference.button(),
+    }
+
+
+async def reference_getter(
+    i18n: TranslatorRunner,
+    **kwargs,
+):
+    return {
+        'reference_text': i18n.reference(
+            time_open=TIME_OPEN, time_close=TIME_CLOSE
+        ),
+        'back_to_main_menu': i18n.back.to.main.menu(),
     }
