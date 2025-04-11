9c55d1db714a75fa46b8ba31b6c3512f6404fe90
mishatunikov <michael.tunikov@gmail.com>
2025-04-11 21:16:23 +0300

    Изменен host, для запуска через docker-compose.

--- fsm/fsm.py
+++ fsm/fsm.py
@@ -1,10 +1,8 @@
-from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.base import DefaultKeyBuilder
-from aiogram.fsm.storage.memory import MemoryStorage
 from redis.asyncio import Redis
 from aiogram.fsm.storage.redis import RedisStorage
 
-redis = Redis(host='localhost', port=6379)
+redis = Redis(host='redis', port=6379)
 storage = RedisStorage(
     redis=redis, key_builder=DefaultKeyBuilder(with_destiny=True)
 )
