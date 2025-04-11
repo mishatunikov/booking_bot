0375e9cb2011945dc43c73253ca435ca0159a78a
mishatunikov <michael.tunikov@gmail.com>
2025-04-11 21:15:57 +0300

    Автоматическое создание папки под бд, нужно под volume.

--- db/connection.py
+++ db/connection.py
@@ -1,4 +1,11 @@
+from pathlib import Path
+
 from sqlalchemy.ext.asyncio import create_async_engine
 
+BASE_DIR = Path(__file__).parent.parent
+path = BASE_DIR / 'database'
 
-engine = create_async_engine('sqlite+aiosqlite:///text.db', echo=True)
+if not path.exists():
+    path.mkdir(parents=True)
+
+engine = create_async_engine(f'sqlite+aiosqlite:///{path}/text.db', echo=True)
