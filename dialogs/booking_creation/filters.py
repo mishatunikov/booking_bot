0d0abb4887ce50005fe4c5ed4e2d9deef2653585
mishatunikov <michael.tunikov@gmail.com>
2025-04-11 22:46:13 +0300

    Изменена проверка ввода.

--- dialogs/booking_creation/filters.py
+++ dialogs/booking_creation/filters.py
@@ -1,17 +1,19 @@
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
 
-from dialogs.booking_creation.consts import MAX_NAME_LENGTH
+from dialogs.booking_creation.consts import MAX_NAME_LENGTH, MIN_NAME_LENGTH
 
 
 class CorrectInput(BaseFilter):
 
     async def __call__(self, message: Message):
 
-        if any(char.isdigit() for char in message.text):
+        if not all(char.isalpha() for char in message.text):
             return False
 
-        if len(message.text) > MAX_NAME_LENGTH:
+        if len(message.text) not in range(
+            MIN_NAME_LENGTH, MAX_NAME_LENGTH + 1
+        ):
             return False
 
         return True
